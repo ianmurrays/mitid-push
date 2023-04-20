4f06b71c6023f9098668201e3f4413806546bea2
Ian Murray <ianmurrays@gmail.com>
2023-04-20 22:41:50 +0200

    Add working PoC

--- src/options/Options.svelte
+++ src/options/Options.svelte
@@ -1,13 +1,50 @@
 <script lang="ts">
-  const crx = 'create-chrome-ext'
+  async function getToken(): Promise<string> {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(['notificationToken'], (result) => {
+        resolve(result.notificationToken);
+      });
+    });
+  }
+
+  function selectAll(event: MouseEvent) {
+    (event.target as HTMLInputElement).select();
+  }
+
+  function regenerateToken() {
+    if (!confirm('Are you sure you want to regenerate your token?')) {
+      return;
+    }
+
+    const notificationToken = crypto.randomUUID();
+
+    chrome.storage.local.set({ notificationToken }, () => {
+      notificationTokenPromise = Promise.resolve(notificationToken);
+    });
+  }
+
+  function testNotification() {
+    chrome.runtime.sendMessage({ type: 'waiting-for-approval' });
+  }
+
+  let notificationTokenPromise: Promise<string> = getToken();
 </script>
 
 <main>
-  <h3>Options Page!</h3>
+  <h3>MitID Push</h3>
 
-  <h6>v 0.0.0</h6>
+  {#await notificationTokenPromise}
+    <input type="text" readonly value="Loading..." />
+  {:then notificationToken}
+    <input type="text" readonly value={notificationToken} on:click={selectAll} />
+    <button on:click={regenerateToken}>Regenerate</button>
+    <button on:click={testNotification}>Test</button>
 
-  <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">Power by {crx}</a>
+    <p>
+      In your <a href="https://ntfy.sh" target="_blank" rel="noopener">ntfy</a> app, add a new subscription using the token above as the topic name.
+    </p>
+  {/await}
+
 </main>
 
 <style>
@@ -23,30 +60,35 @@
   }
 
   h3 {
-    color: #ff3e00;
-    text-transform: uppercase;
+    color: #0060e6;
     font-size: 1.5rem;
-    font-weight: 200;
+    font-weight: bold;
     line-height: 1.2rem;
     margin: 2rem auto;
   }
 
-  h6 {
-    font-size: 0.5rem;
-    color: #333333;
-    margin: 0.5rem;
+  input {
+    width: 100%;
+    max-width: 20rem;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    margin: 0 auto;
+    text-align: center;
   }
 
-  a {
-    font-size: 0.5rem;
-    margin: 0.5rem;
-    color: #cccccc;
-    text-decoration: none;
-  }
-
-  @media (min-width: 480px) {
-    h3 {
-      max-width: none;
-    }
+  button {
+    /* display: block; */
+    margin: 1rem auto;
+    padding: 0.5rem 1rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: #fff;
+    background-color: #0060e6;
+    cursor: pointer;
   }
 </style>
